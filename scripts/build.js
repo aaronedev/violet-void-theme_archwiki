5f2efbab9b0a61f0a8acf73664f34b30dc703241
aaronedev <mail@aaronsamuel.de>
2025-12-19 18:39:28 +0100

    feat: added first version of new nav

--- scripts/build.js
+++ scripts/build.js
@@ -1,26 +1,26 @@
-const fs = require('fs');
-const path = require('path');
-const stylus = require('stylus');
-const autoprefixer = require('autoprefixer-stylus');
-const pkg = require('../package.json');
+const fs = require('fs')
+const path = require('path')
+const stylus = require('stylus')
+const autoprefixer = require('autoprefixer-stylus')
+const pkg = require('../package.json')
 
-const inputFile = path.join(__dirname, '../src/main.styl');
-const outputFile = path.join(__dirname, '../dist/main.css');
-const distDir = path.dirname(outputFile);
-const pkgFile = path.join(__dirname, '../package.json');
+const inputFile = path.join(__dirname, '../src/main.styl')
+const outputFile = path.join(__dirname, '../dist/main.css')
+const distDir = path.dirname(outputFile)
+const pkgFile = path.join(__dirname, '../package.json')
 
 // Bump version
-const now = new Date();
-const pad = (n) => n.toString().padStart(2, '0');
-const version = `${now.getFullYear()}${pad(now.getMonth() + 1)}${pad(now.getDate())}.${pad(now.getHours())}.${pad(now.getMinutes())}`;
+const now = new Date()
+const pad = (n) => n.toString().padStart(2, '0')
+const version = `${now.getFullYear()}${pad(now.getMonth() + 1)}${pad(now.getDate())}.${pad(now.getHours())}.${pad(now.getMinutes())}`
 
-pkg.userStyle.version = version;
-fs.writeFileSync(pkgFile, JSON.stringify(pkg, null, 2) + '\n');
-console.log(`Bumped version to ${version}`);
+pkg.userStyle.version = version
+fs.writeFileSync(pkgFile, JSON.stringify(pkg, null, 2) + '\n')
+console.log(`Bumped version to ${version}`)
 
 // Create dist dir if not exists
 if (!fs.existsSync(distDir)) {
-  fs.mkdirSync(distDir, { recursive: true });
+  fs.mkdirSync(distDir, { recursive: true })
 }
 
 // Generate UserStyle header
@@ -35,26 +35,26 @@ const header = `/* ==UserStyle==
 @license      ${pkg.license}
 ==/UserStyle== */
 
-`;
+`
 
-const stylContent = fs.readFileSync(inputFile, 'utf8');
+const stylContent = fs.readFileSync(inputFile, 'utf8')
 
-console.log('Building CSS...');
+console.log('Building CSS...')
 
 stylus(stylContent)
   .set('filename', inputFile)
   .use(autoprefixer())
   .render((err, css) => {
     if (err) {
-      console.error('Error building CSS:', err);
-      process.exit(1);
+      console.error('Error building CSS:', err)
+      process.exit(1)
     }
-    
+
     // Check if the original @document block was preserved (Stylus usually handles it)
     // If we want to move the @document block *after* the UserStyle header (which we do),
     // we just prepend the header.
-    
-    const finalCss = header + css;
-    fs.writeFileSync(outputFile, finalCss);
-    console.log(`Build complete: ${outputFile}`);
-  });
+
+    const finalCss = header + css
+    fs.writeFileSync(outputFile, finalCss)
+    console.log(`Build complete: ${outputFile}`)
+  })
